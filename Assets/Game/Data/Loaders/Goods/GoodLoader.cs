af5834943acdeeeb44ef810e9a5065b751e5d013
Alex VdB <alexvdbr6@gmail.com>
2026-02-11 20:42:59 +0100

    feat: Good type loader + test

--- Assets/Game/Data/Loaders/Goods/GoodLoader.cs
+++ Assets/Game/Data/Loaders/Goods/GoodLoader.cs
@@ -1,17 +1,13 @@
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Xml;
+
 
 
 
 public class GoodLoader  
 {
-
     private struct GoodData
     {
         public string name;
---
+++ Assets/Game/Tests/EditMode/Loader/GoodTypeLoaderTest.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodTypeLoaderTest
+{
+    [Test]
+    public void GoodTypeLoaderDeserialize_goodsType_True()
+    {
+        GoodTypeLoader loader = new GoodTypeLoader();
+        string json = @"
+            [
+            ""Raw"",
+            ""Manifactured"",
+            ""Luxury"",
+            ""Military"",
+            ]";
+
+        Dictionary<int,string> result = loader.Deserialize_goodsType(json);
+
+        Assert.AreEqual(4, result.Count);
+        Assert.AreEqual("Raw", result[0]);
+        Assert.AreEqual("Manifactured", result[1]);
+        Assert.AreEqual("Luxury", result[2]);
+        Assert.AreEqual("Military", result[3]);
+
+    }
+}
