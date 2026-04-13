f2aab8a7cf065704576b915758b8900b6d0c5b0c
Alex VdB <alexvdbr6@gmail.com>
2026-04-13 22:11:45 +0200

    feat: pathfinding terrain cost test

--- Assets/Game/Tests/EditMode/Simulation/PathFinding/PathindingTest.cs
+++ Assets/Game/Tests/EditMode/Simulation/PathFinding/PathindingTest.cs
@@ -6,8 +6,7 @@ using UnityEngine;
 using static ColorUtilities;
 public class PathindingTest {
     
-    GraphDataLoder graphLoader = new GraphDataLoder();
-    TileLoader tileLoader = new TileLoader();
+ 
     private string graphTestJsonPath = Path.Combine(
          Application.dataPath,
          "Game",
@@ -16,10 +15,17 @@ public class PathindingTest {
          "Simulation",
          "PathFinding",
          "mapJson");
-
-
+    private GraphDataLoder graphLoader;
+    private TileLoader tileLoader;
+    [SetUp]
+    public void Setup()
+    {
+        graphLoader = new GraphDataLoder();
+        tileLoader = new TileLoader();
+    }
     public DataRegistery GetGraph01()
     {
+
         IResolutionErrorHandler _errorHandler = new ThrowErrorHandler();
         string jsontest01Path = Path.Combine(graphTestJsonPath, "01");
 
@@ -69,7 +75,39 @@ public class PathindingTest {
         return _registery;
     }
 
+    public DataRegistery  GetGraph03()
+    {
+        IResolutionErrorHandler _errorHandler = new ThrowErrorHandler();
+        string jsontest03Path = Path.Combine(graphTestJsonPath, "03");
 
+        string tileGraphJson = GetJsonStringFromFile(jsontest03Path, "tileGraph");
+        string tileDataJson = GetJsonStringFromFile(jsontest03Path, "tileData");
+
+        DataRegistery _registery = new DataRegistery();
+
+        TerrainType terrainType = new TerrainType();
+        terrainType.name = "Default";
+        terrainType.tag = "Default";
+        terrainType.isLandType = true;
+        terrainType.movementCost = 1;
+
+        TerrainType terrainType2 = new TerrainType();
+        terrainType2.name = "Costly";
+        terrainType2.tag = "Costly";
+        terrainType2.isLandType = true;
+        terrainType2.movementCost = 1000; // Is in %
+
+        TerrainType[] array = { terrainType, terrainType2 };
+        _registery.terrainTypes = array;
+
+        _registery.terrainTypesTags = new string[] { "Default", "Costly" };
+
+        TerrainType test = _registery.GetTerrainTypeById(_registery.GetTerrainTypes("Default"));
+
+        _registery.tiles = tileLoader.DeserializeTiles(tileDataJson, _registery, _errorHandler);
+        _registery = graphLoader.Load(tileGraphJson, _registery, _errorHandler);
+        return _registery;
+    }
     [Test]
     public void Pathfinder_outputRightAmmountOfNode()
     {
@@ -124,6 +162,24 @@ public class PathindingTest {
         //{
         //    Debug.Log(node.position);
         //}
+    }
+    [Test]
+    public void PathFinder_MovementCost_return7()
+    {
+
+        DataRegistery _registery = GetGraph03();
+        Pathfinding pathfinder = new Pathfinding(3);
+        MapGraphNode start = _registery.GetMapGraphNode(HexToInt("#0BB235"));
+        MapGraphNode end = _registery.GetMapGraphNode(HexToInt("#716cd6"));
+        List<MapGraphNode> result = pathfinder.FindPath(start, end, new List<UnitTerrainSpeedModdifier>(), _registery);
+
+        foreach (MapGraphNode node in result)
+        {
+            Debug.Log(node.position);
+        }
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(7, result.Count);//start node is included
     }
 
     //MapGraph a = new MapGraph()
