4636bf6e9d12132ca9a2aca0b09d72d030f49673
Alex VdB <alexvdbr6@gmail.com>
2026-04-19 21:03:43 +0200

    wip: registery builder

--- Assets/Game/Simulation/Databases/DataRegistery_Builder.cs
+++ Assets/Game/Simulation/Databases/DataRegistery_Builder.cs
@@ -1,30 +1,31 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DataRegistery_Builder
 {
-    public string[] popStrata { get; set; } = new string[] { "Default" };
-    public string[] goodTypes { get; set; } = new string[] { "Default" };
+    public string[] popStrata { get; set; }
+    public string[] goodTypes { get; set; }
     public Good[] goodList { get; set; }
 
-    public string[] countriesTag { get; set; } = new string[] { "Default" };
+    public string[] countriesTag { get; set; }
 
-    public string[] terrainTypesTags { get; set; } = new string[] { "Default" };
+    public string[] terrainTypesTags { get; set; }
     public TerrainType[] terrainTypes { get; set; } = new TerrainType[] { };
-    public string[] climateTypesTags { get; set; } = new string[] { "Default" };
+    public string[] climateTypesTags { get; set; }
     public Dictionary<string, int> rgoTag = new Dictionary<string, int>
     {
         { "Default", 0 },
     };
     //TODO To change initialisation of default
-    private static readonly Culture _default = new Culture("default", "default");
+    private static readonly Culture _default;
 
     public Culture[] cultures = new Culture[] { _default };
     public PopJob[] popJobs;
     public Religion[] religionsDef;
     public Dictionary<string, GoodNeedMax[]> strataNeeds;
-    public string[] provinceTag = new string[] { "Default" };
+    public string[] provinceTag;
     public Dictionary<int, Tile> tiles = new Dictionary<int, Tile>();
 
     public List<MapGraphNode> mapGraphNodes;
@@ -51,7 +52,7 @@ public class DataRegistery_Builder
         this.provinceTag = provinceTags;
         return this;
     }
-    public DataRegistery_Builder WithTileDictionary(Dictionary<int,Tile> tileDict)
+    public DataRegistery_Builder WithTileDictionary(Dictionary<int, Tile> tileDict)
     {
         this.tiles = tileDict;
         return this;
@@ -66,7 +67,7 @@ public class DataRegistery_Builder
         this.climateTypesTags = climateTypes;
         return this;
     }
-    public DataRegistery_Builder WithRgoTag(Dictionary<string,int> rgo)
+    public DataRegistery_Builder WithRgoTag(Dictionary<string, int> rgo)
     {
         this.rgoTag = rgo;
         return this;
@@ -101,7 +102,7 @@ public class DataRegistery_Builder
         this.mapUnitState = state;
         return this;
     }
-    public DataRegistery_Builder WithMapUnitDict(Dictionary<int,UnitNavigation> dict)
+    public DataRegistery_Builder WithMapUnitDict(Dictionary<int, UnitNavigation> dict)
     {
         this.mapUnitDict = dict;
         return this;
@@ -109,6 +110,73 @@ public class DataRegistery_Builder
 
     public DataRegistery Build()
     {
+        DataRegistery registery = new DataRegistery();
+        string[] popStrata = new string[] { "Default" };
+        registery.popStrata = popStrata.Concat(this.popStrata).ToArray();
+        string[] goodtypes = new string[] { "Default" };
+        registery.goodTypes = goodtypes.Concat(this.goodTypes).ToArray();
+
+        Good[] goods = new Good[this.goodList.Length + 1];
+        goods[0] = CreateDefaultGood();
+        registery.goodList = goods.Concat(this.goodList).ToArray();
+        string[] countryTags = new string[] { "Default" };
+        registery.countriesTag = countryTags.Concat(this.countriesTag).ToArray();
+
+
+        string[] terrainTypeTags = new string[] { "Default" };
+        registery.terrainTypesTags = terrainTypeTags.Concat(this.terrainTypesTags).ToArray();
+
+        TerrainType[] terrainTypes = new TerrainType[this.terrainTypes.Length + 1];
+        terrainTypes[0] = CreateDefaultTerrainType();
+
+        registery.terrainTypes = terrainTypes.Concat(this.terrainTypes).ToArray() ;
+
         throw new NotImplementedException();
+    }
+
+    private Good CreateDefaultGood()
+    {
+
+        Good defaultGood = new Good();
+        defaultGood.id = 0;
+        defaultGood.name = "Default";
+        defaultGood.tag = "Default";
+        defaultGood.type = 0;
+        defaultGood.basePrice = 0;
+        defaultGood.weight = 0;
+        defaultGood.baseProductionModdifier = 0;
+        defaultGood.iconPath = "default";
+        defaultGood.color = "#808080";
+        return defaultGood;
+    }
+
+    private TerrainType CreateDefaultTerrainType()
+    {
+        TerrainType defaultTerrain = new TerrainType();
+        defaultTerrain.isLandType = true;
+        defaultTerrain.tag = "def";
+        defaultTerrain.name = "Default";
+        defaultTerrain.movementCost = 1;
+        return defaultTerrain;
+    }
+
+    private Culture CreateDefaultCulture()
+    {
+        string tag = "Default";
+        Culture defaultCulture = new Culture(tag, tag);
+        return defaultCulture;
+    }
+
+    private PopJob CreateDefaultPopJob()
+    {
+        string tag = "Default";
+        PopJob defaultPopJob = new PopJob(tag, 0, tag);
+        return defaultPopJob;
+    }
+
+    private Religion CreateDefaultReligion() {
+        string tag = "Default";
+        Religion defaultReligion = new Religion(tag, tag);
+        return defaultReligion;
     }
 }
--- Assets/Game/Simulation/Population/Religion.cs
+++ Assets/Game/Simulation/Population/Religion.cs
@@ -4,4 +4,10 @@ public class Religion : IHaveTag
 {
     public string tag { get; set; }
     public string name;
+
+    public Religion(string tag, string name)
+    {
+        this.tag = tag;
+        this.name = name;
+    }
 }
